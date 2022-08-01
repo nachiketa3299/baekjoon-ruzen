de91297175c1334c367d2f994190d42fca7aadef
RUZEN <nachiketa3299@gmail.com>
2022-08-01 16:07:49 +0900

    Initialized repository

---
+++ 10171.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+\    /\
+ )  ( ')
+(  /  )
+ \(__)|
+*/
+
+
+int main (void) {
+    cout << "\\    /\\" << endl;
+    cout << " )  ( \')" << endl;
+    cout << "(  /  )" << endl;
+    cout << " \\(__)|" << endl;
+    return 0;
+}
+
+// TODO: Escaping letters에 대한 조사
---
+++ 10718.py
@@ -0,0 +1,6 @@
+def solution ():
+    str = ["강한친구", "대한육군"]
+    for i in range(2):
+        print(f"{str[0]} {str[1]}")
+
+solution()
