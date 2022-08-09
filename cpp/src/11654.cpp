3498a0d9f21e92c008c45b026f4bfc83e8d76fc1
Ruzen <nachiketa3299@gmail.com>
2022-08-09 22:13:46 +0900

    함수 문제풀이 완료~

---
+++ cpp/src/11654.cpp
@@ -0,0 +1,13 @@
+// https://www.acmicpc.net/problem/11654
+// 2022-08-10
+
+#include <iostream>
+
+using namespace std;
+
+int main (void) {
+    cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    
+    return 0;
+}
